320df1b755aa1ce6b280115284fa8a9f9f22485d
masatonandate <masatonandate@gmail.com>
2022-07-14 00:05:06 -0700

    Updates

---
+++ precache-manifest.75f2a5110f004c192185aa74d6e2231e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "04ee8c5e1e76a7d767ef",
+    "url": "/spotifun/static/css/main.43612129.chunk.css"
+  },
+  {
+    "revision": "04ee8c5e1e76a7d767ef",
+    "url": "/spotifun/static/js/main.1e95f976.chunk.js"
+  },
+  {
+    "revision": "8f7db0dcd81afdbf50c8",
+    "url": "/spotifun/static/js/runtime~main.582706ea.js"
+  },
+  {
+    "revision": "424bb6edff9d3be9aa4b",
+    "url": "/spotifun/static/js/2.9cc21b70.chunk.js"
+  },
+  {
+    "revision": "f58fcdc5ac8f61bbdaf61679d36a2b6c",
+    "url": "/spotifun/static/media/FullVinyl6.f58fcdc5.png"
+  },
+  {
+    "revision": "d7b9b7d403385acffd6abeaa0c6a5613",
+    "url": "/spotifun/static/media/shelf2.d7b9b7d4.png"
+  },
+  {
+    "revision": "92e3b4890de587be7c55ef1125e42ddd",
+    "url": "/spotifun/index.html"
+  }
+];
