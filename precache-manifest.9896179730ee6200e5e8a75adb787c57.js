cd52e3477147b838eff7b31b88b5a136792aa835
masatonandate <masatonandate@gmail.com>
2022-08-01 18:25:07 -0700

    Updates

---
+++ precache-manifest.9896179730ee6200e5e8a75adb787c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96aed780b7650d6c3e4b",
+    "url": "/spotifun/static/css/main.efb99481.chunk.css"
+  },
+  {
+    "revision": "96aed780b7650d6c3e4b",
+    "url": "/spotifun/static/js/main.31388073.chunk.js"
+  },
+  {
+    "revision": "8f7db0dcd81afdbf50c8",
+    "url": "/spotifun/static/js/runtime~main.582706ea.js"
+  },
+  {
+    "revision": "84c200aade4770628f26",
+    "url": "/spotifun/static/js/2.5083bf33.chunk.js"
+  },
+  {
+    "revision": "b82f86087396fdb59db6ace74de21a6e",
+    "url": "/spotifun/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spotifun/precache-manifest.9e14f043bb1c6dc99b7bd27cde008365.js"
+  "/spotifun/precache-manifest.9896179730ee6200e5e8a75adb787c57.js"
 );
 
 workbox.clientsClaim();
